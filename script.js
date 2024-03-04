f1856eb7db538b195517d141b5926526206db860
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-03-05 02:56:24 +0530

    coding challenge 2 done

--- script.js
+++ script.js
@@ -207,3 +207,40 @@ const wait = function (second) {
 //   console.log('Print after 4 second')
 //   return wait(1)
 // })
+
+const imageContainer = document.querySelector('.images')
+
+const createImg = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('load', function () {
+      imageContainer.append(img);
+      resolve(img);
+    })
+    img.addEventListener('error', function () {
+      reject(new Error('Image Not Found'))
+    });
+
+
+  })
+}
+let currentImage;;
+createImg('img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    console.log('Image 1 loaded')
+    return wait(2)
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImg('img/img-2.jpg')
+  }).then((img) => {
+    currentImage = img;
+    console.log('Image 2 loaded')
+    return wait(2)
+  }).then(() => {
+    currentImage.style.display = 'none';
+  }).catch(err => {
+    console.error(err)
+  })
